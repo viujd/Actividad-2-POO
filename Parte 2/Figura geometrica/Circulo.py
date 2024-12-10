071d86634ef2993755cc6b50834545dc4a72aa28
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2024-12-10 18:32:29 -0500

    Create Circulo.py

---
+++ Parte 2/Figura geometrica/Circulo.py
@@ -0,0 +1,11 @@
+import math
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+
+    def calcular_area(self):
+        return math.pi * (self.radio ** 2)
+
+    def calcular_perimetro(self):
+        return 2 * math.pi * self.radio
