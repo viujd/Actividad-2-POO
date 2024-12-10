9c6f72a0ae2a63d7b723267d823a03e59a99f39d
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2024-12-10 18:38:33 -0500

    Create TrianguloRectangulo.py

---
+++ Parte 2/Figura geometrica/TrianguloRectangulo.py
@@ -0,0 +1,15 @@
+import math
+
+class TrianguloRectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return (self.base * self.altura) / 2
+
+    def calcular_perimetro(self):
+        return self.base + self.altura + self.calcular_hipotenusa()
+
+    def calcular_hipotenusa(self):
+        return math.sqrt(self.base ** 2 + self.altura ** 2)
