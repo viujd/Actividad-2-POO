af7ede918ddbdaec62b14cd584dc1c193cf9554f
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2024-12-10 18:35:45 -0500

    Create Rectangulo.py

---
+++ Parte 2/Figura geometrica/Rectangulo.py
@@ -0,0 +1,10 @@
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+    def calcular_perimetro(self):
+        return (2 * self.base) + (2 * self.altura)
